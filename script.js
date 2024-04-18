8be5651f3a1da4645b6e0a31efcb9dd6d71fd67e
portelaaa <e107ti@etec.sp.gov.br>
2024-04-18 20:03:06 -0300

    JS finalizado

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Olá usuário!");
+
+let nome = prompt("Qual é o seu nome?");
+let sobrenome = prompt("Qual é o seu sobrenome?");
+
+alert("Nome Completo");
+alert(nome + " " + sobrenome);
+
+alert("Nome Catálogo");
+alert(sobrenome.toUpperCase() + ", " + nome);
+
